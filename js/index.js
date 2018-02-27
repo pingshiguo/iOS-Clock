02b13d2ed0c3cf05ff3cc29a049d1c7325637398
PingShiGuo <pingshiguo@gmail.com>
2018-02-27 17:56:47 +0800

    update

--- js/index.js
+++ js/index.js
@@ -1,20 +1,45 @@
+const scaleHand = document.querySelector('.scale-wrapper');
 const hourHand = document.querySelector('.hours-wrapper');
 const minuteHand = document.querySelector('.minutes-wrapper');
 const secondHand = document.querySelector('.seconds-wrapper');
 
-function RunClock() {
+drawClockScale();
+runClock();
+setInterval(runClock, 1000);
+
+function runClock() {
   let now = new Date();
   let hour = now.getHours();
   let minute = now.getMinutes();
   let second = now.getSeconds();
 
-  let secondDegree = second / 60 * 360 - 180;
-  let minuteDegree = (minute / 60 * 360) + (second / 60 * 6) - 180;
-  let hourDegree = (hour / 12 * 360) + (minute / 60 * 30) - 180;
+  let secondDeg = second / 60 * 360 - 180;
+  let minuteDeg = (minute / 60 * 360) + (second / 60 * 6) - 180;
+  let hourDeg = (hour / 12 * 360) + (minute / 60 * 30) - 180;
 
-  secondHand.style.transform = `rotateZ(${secondDegree}deg)`;
-  minuteHand.style.transform = `rotateZ(${minuteDegree}deg)`;
-  hourHand.style.transform = `rotateZ(${hourDegree}deg)`;
+  secondHand.style.transform = `rotateZ(${secondDeg}deg)`;
+  minuteHand.style.transform = `rotateZ(${minuteDeg}deg)`;
+  hourHand.style.transform = `rotateZ(${hourDeg}deg)`;
 }
 
-setInterval(RunClock, 1000);
+function drawClockScale() {
+  const ORIGIN_X = 180;
+  const ORIGIN_Y = 180;
+  const R = 150;
+  const DEG = Math.PI * 2 / 12;
+
+  for (let i = 1; i <= 12; i++) {
+    let angle = DEG * i;
+    let x = ORIGIN_X + Math.cos(angle) * R;
+    let y = ORIGIN_Y + Math.sin(angle) * R;
+
+    let scale = document.createElement('i');
+
+    scale.innerText = i;
+    scale.className = 'scale';
+    scale.style.top = y + 'px';
+    scale.style.left = x + 'px';
+
+    scaleHand.appendChild(scale);
+  }
+}
